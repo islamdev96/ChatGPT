3e4495a3bce7d2f12f5e28f1f57ef827761d85ba
islamdev96 <islamsayedglab@gmail.com>
2023-03-16 21:58:30 +0200

    first commit

---
+++ lib/constants/api_consts.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: non_constant_identifier_names
+
+String BASE_URL = "https://api.openai.com/v1";
+String API_KEY = "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";
+
+// String BASE_URL = "https://api.openai.com/v1";
+
+
+// String API_KEY = "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";
+// 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
