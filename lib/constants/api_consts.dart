db191fd3620aa8b33432496d449cf7c1018b1b11
islamdev96 <islamsayedglab@gmail.com>
2023-03-24 16:54:22 +0200

    xs

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: non_constant_identifier_names
 
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
 
 // String BASE_URL = "https://api.openai.com/v1";
 
 
 // String API_KEY = "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";
-// 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
+// String API_KEY = "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";
+// String API_KEY = "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";
+
